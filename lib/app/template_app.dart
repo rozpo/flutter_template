96ebab599a5504f9be9ae3b18331209c07cbfa98
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-01-08 18:58:19 +0000

    Add main file

---
+++ lib/app/template_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TemplateApp extends StatelessWidget {
+  const TemplateApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'app/template_app.dart';
+
+void main() {
+  runApp(TemplateApp());
+}
